484df799a8b690e1b2974988537fec9c5772452f
PratikAcharya45 <acharyapratik332@gmail.com>
2024-04-28 11:07:43 +0545

    Home Page

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:[" Web Developer"," Software Developer"," Data Analyst"," Sports Enthusiast"],
+    typeSpeed:100,
+    backSpeed:20,
+    backDelay:500,
+    loop:true
+})
